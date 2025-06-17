b205c97a2ad8dffc547a1a5b33dd53e8173f556d
Sessobin <mohantykrishna57@gmail.com>
2025-06-17 16:23:51 +0530

    initialied landing page

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Navbar from '@/src/components/navbars'
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main>
+        <h1>BlinkChat</h1>
+        <Navbar/>
+      </main>
+    </div>
+  );
+}
