b2bdcdb8efef3c246c3e1f9c2c941e8050cfefcb
Sessobin <mohantykrishna57@gmail.com>
2025-06-19 16:35:40 +0530

    Update layout and add HeroSection and Contactus components

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,16 +1,15 @@
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
+import HeroSection from '@/components/HeroSection';
+import Contactus from '@/components/Contactus';
 import './globals.css';
+
 export default function Home() {
   return (
     <>
       <Navbar />
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="flex flex-col items-center justify-center flex-1 p-4">
-        <h1 className="text-4xl font-bold text-gray-900">Welcome to BlinkChat</h1>
-        <p className="mt-2 text-lg text-gray-700">Your real-time chat solution.</p>
-      </div>
-      </main>
+        <HeroSection/>
+        <Contactus/>
       <Footer />
       </>
   );
